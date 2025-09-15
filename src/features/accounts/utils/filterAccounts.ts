c5400855068fad9bfac5caff0c8095d34f1d532c
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-15 15:10:57 +0200

    replace new account interface and fix folder location

---
+++ src/features/accounts/utils/filterAccounts.ts
@@ -0,0 +1,19 @@
+import type { Account } from "../types/accountTypes";
+import type {
+  SearchDates, 
+  SelectableItem,
+} from "../../../types";
+
+export function filterAccounts(
+  accounts: Account[],
+  type: SelectableItem[],
+  currency: SelectableItem[],
+  { min, max }: SearchDates,
+) {
+  return accounts.filter((acc) => 
+    (min === null || +acc.lastUpdated >= min) &&
+    (max === null || +acc.lastUpdated <= max) &&
+    type[acc.type].status &&
+    currency[acc.currency].status
+  );
+}
