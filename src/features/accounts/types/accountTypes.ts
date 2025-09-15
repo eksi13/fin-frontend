da53fe8d8f635fa8d74044dacbe24ee2444c19a1
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-15 15:08:19 +0200

    add new interface for raw account from api response and add accoutn interface with date type

---
+++ src/features/accounts/types/accountTypes.ts
@@ -0,0 +1,12 @@
+export interface RawAccount {
+  id: number;
+  name: string;
+  balance: number;
+  type: number;
+  currency: number;
+  lastUpdated: string;
+}
+
+export interface Account extends Omit<RawAccount, 'lastUpdated'> {
+  lastUpdated: Date;
+};
