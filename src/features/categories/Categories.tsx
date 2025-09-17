e99ea7a1b6020875e38e2119a1da4090c8dad19e
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-17 15:08:17 +0200

    moved fetching in component

--- src/features/categories/Categories.tsx
+++ src/features/categories/Categories.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react';
+// React
+import { useEffect, useState } from 'react';
 
+// Types
+import type { Category } from './types/categoryTypes';
 import type { SearchAmounts, SearchDates, SelectableItem } from '../../types';
 
+// Utilities / Services
+import { filterCategories } from './utils/filterCategories';
+import { fetchCategories } from './services/categoryService';
+
+// Components
 import Items from '../../components/Items';
 import SearchSection from '../../components/search/SearchSection';
-import type { Category } from './types/categoryTypes';
-import { filterCategories } from './utils/filterCategories';
 
-type Props = {
-  categories: Category[];
-  errorMessage: string;
-};
+type Props = {};
 
-function Categories({ categories, errorMessage }: Props) {
+function Categories({}: Props) {
   const [categoryTypeSearch, setCategoryTypeSearch] = useState<
     SelectableItem[]
   >([
@@ -30,6 +33,22 @@ function Categories({ categories, errorMessage }: Props) {
     max: Date.now(),
   });
 
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [fetchError, setfetchError] = useState<string>('');
+  const API_URL = String(import.meta.env.VITE_API_URL + 'categories');
+
+  useEffect(() => {
+    fetchCategories(API_URL)
+      .then((categories) => {
+        setCategories(categories || []);
+      })
+      .catch((err: unknown) => {
+        err instanceof Error
+          ? setfetchError(err.message)
+          : setfetchError('unknown error');
+      });
+  }, []);
+
   return (
     <>
       <Items
@@ -40,7 +59,7 @@ function Categories({ categories, errorMessage }: Props) {
           searchDates,
           searchAmounts
         )}
-        errorMessage={errorMessage}
+        errorMessage={fetchError}
       />
       <SearchSection
         setSearchDates={[setSearchDates]}
