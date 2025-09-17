b7971c0a05dd9134ceb025f7b3ac70578afa1270
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-17 15:12:08 +0200

    refactor fetching into compontents

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,25 @@
+// Global styles
+import './styles/index.css';
+
+// React / Router libraries
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+// Layout components
 import Header from './components/layout/Header';
 import Nav from './components/layout/Nav';
-import Home from './components/Home';
 
-import { transactions } from './features/transactions/types/dummyTransactions';
-import { type Account } from './features/accounts/types/accountTypes';
+// Page components
+import Home from './components/Home';
 import Transactions from './features/transactions/Transactions';
 import Accounts from './features/accounts/Accounts';
 import Categories from './features/categories/Categories';
-import { useEffect, useState } from 'react';
-import { fetchAccounts } from './features/accounts/services/accountService';
-import type { Category } from './features/categories/types/categoryTypes';
-import { fetchCategories } from './features/categories/services/categoryService';
+import Dashboard from './components/Dashboard';
+
+// Types
+
+// Services
 
 function App() {
-  const API_URL = 'http://localhost:3000/';
-
-  const tas = transactions;
-
-  const [accounts, setAccounts] = useState<Account[]>([]);
-  const [categories, setCategories] = useState<Category[]>([]);
-
-  const [fetchErrorAccount, setfetchErrorAccount] = useState<string>('');
-  const [fetchErrorCategory, setfetchErrorCategory] = useState<string>('');
-
-  useEffect(() => {
-    fetchAccounts(API_URL + "accounts")
-      .then((accounts) => {
-        setAccounts(accounts || []);
-      })
-      .catch((err: unknown) => {
-        err instanceof Error
-          ? setfetchErrorAccount(err.message)
-          : setfetchErrorAccount('unknown error');
-      });
-    fetchCategories(API_URL + "categories")
-      .then((categories) => {
-        setCategories(categories || []);
-      })
-      .catch((err: unknown) => {
-        err instanceof Error
-          ? setfetchErrorCategory(err.message)
-          : setfetchErrorCategory('unknown error');
-      });
-  }, []);
-
   return (
     <div>
       <Header title=" " />
@@ -53,22 +27,12 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route
-            path="/transactions"
-            element={<Transactions transactions={tas} />}
-          />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
           <>
-            <Route
-              path="/accounts"
-              element={
-                <Accounts accounts={accounts} errorMessage={fetchErrorAccount} />
-              }
-            />
+            <Route path="/accounts" element={<Accounts />} />
           </>
-          <Route
-            path="/categories"
-            element={<Categories categories={categories} errorMessage={fetchErrorCategory} />}
-          />
+          <Route path="/categories" element={<Categories />} />
         </Routes>
       </BrowserRouter>
     </div>
