d2e70d8fb071a7a039b74df7400c08a19f243751
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-17 14:36:28 +0200

    draft dashboards

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+type Props = {};
+
+function Dashboard({}: Props) {
+  return (
+    <>
+      <h2 className="title">{'Dashboard'}</h2>
+      <p className="error">{'To be implemented'}</p>
+    </>
+  );
+}
+
+export default Dashboard;
